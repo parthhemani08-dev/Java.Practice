0174cc4e0973afbd42f8a214970949f609763edc
Parth Hemani <parthhemani08@gmail.com>
2026-06-14 15:06:03 +0530

    add call by value example

--- callbyvalue.java
+++ callbyvalue.java
@@ -1,5 +1,21 @@
-
-
+import java.util.*;
 public class callbyvalue {
+    public static void swap(int a, int b) {
+        int temp = b;
+        b = a;
+        a = temp;
+        System.out.println("a is : " + a);
+        System.out.println("b is : " + b);
+
+
+    }
     
+    
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        swap(a , b);
+    }
 }
